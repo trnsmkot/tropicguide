d1cdc99094a210fddb2981df779c1d24fc97b552
Vladislav Kasatkin <vlad.n.kasatkin@gmail.com>
2018-12-21 15:59:22 +0700

    Initial Commit

---
+++ TropicGuide/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TropicGuide
+//
+//  Created by Vladislav Kasatkin on 21/12/2018.
+//  Copyright © 2018 Vladislav Kasatkin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
